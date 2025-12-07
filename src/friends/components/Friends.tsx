e849515a305e301dd7997df7261aab78e90d1d97
resAsus <respectedbro@yandex.ru>
2025-12-07 10:22:45 +0300

    add friends

---
+++ src/friends/components/Friends.tsx
@@ -0,0 +1,7 @@
+export const Friends = () => {
+    return (
+        <div className="container">
+            Друзяки
+        </div>
+    );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import App from './App.tsx';
 import Login from './auth/components/Login/Login.tsx';
 import Signup from './auth/components/Signup/Signup.tsx';
+import {Friends} from '@/friends/components/Friends.tsx';
 import AuthRoute from './auth/components/AuthRoute/AuthRoute.tsx';
 import '@/firebase/config.ts'
 
@@ -17,6 +18,7 @@ createRoot(document.getElementById('root')!).render(
                 <Route path='/' element={<AuthRoute><App/></AuthRoute>}/>
                 <Route path='/login' element={<Login/>}/>
                 <Route path='/signup' element={<Signup/>}/>
+                <Route path='/friends' element={<Friends/>}/>
                 <Route path='*' element={<Navigate to='/'/>}/>
             </Routes>
         </BrowserRouter>
