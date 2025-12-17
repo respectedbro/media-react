f861169eaadc8a191b5184e7e8cafd0c79ad708c
NikDm <137294693+respectedbro@users.noreply.github.com>
2025-12-17 15:40:37 +0300

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,12 +3,12 @@ import {getAuth} from 'firebase/auth'
 import {getFirestore} from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBggp5ScfXt-4nXdY-wUScRCXguJ_-p0F0",
-    authDomain: "social-m-af08b.firebaseapp.com",
-    projectId: "social-m-af08b",
-    storageBucket: "social-m-af08b.firebasestorage.app",
-    messagingSenderId: "567079209522",
-    appId: "1:567079209522:web:6f8a11e91f9cc532bd0a2b"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ''
 };
 
 
