ca913b45b391abdf6fba6cd384547cc5ec8c0929
resAsus <respectedbro@yandex.ru>
2025-12-17 17:10:11 +0300

    add hook add/del friends

--- src/profile/components/ProfileInfo/ProfileInfo.tsx
+++ src/profile/components/ProfileInfo/ProfileInfo.tsx
@@ -3,10 +3,11 @@ import {type UserData} from '@/types/types.ts';
 
 interface ProfileInfoProps {
     userData: UserData | null
+    postsCount: number
 }
 
 
-export const ProfileInfo = ({userData}:ProfileInfoProps) => {
+export const ProfileInfo = ({userData, postsCount}:ProfileInfoProps) => {
     return (
         <div className={styles.profileSection}>
             <div className={styles.profileAvatar}>
@@ -25,7 +26,7 @@ export const ProfileInfo = ({userData}:ProfileInfoProps) => {
                 <div className={styles.profileStats}>
 
                     <div className={styles.statItem}>
-                        <span className={styles.statNumber}>0</span>
+                        <span className={styles.statNumber}>{postsCount}</span>
                         <span className={styles.statLabel}>Постов</span>
                     </div>
                     <div className={styles.statItem}>
